d101b8d92c3e77693df18136401051cc3f4b837c
fabiogunther <86040378+fabiogunther@users.noreply.github.com>
2023-06-09 12:13:44 +0000

    new

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require('express')
+const http = require("https");
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.all('/getDouble/', (req, res) => {
+    getDouble()
+    res.send('Yo!')
+})
+
+function getDouble(){
+    const url = 'https://blaze.com/api/roulette_games/history?startDate=2023-05-10T12:08:43.084Z&endDate=2023-06-09T12:08:43.084Z&page=1';
+
+    let result = '';
+    const req = http.request(url, (res) => {
+        console.log(res.statusCode);
+
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            result += chunk;
+        });
+
+        res.on('end', () => {
+            console.log(result);
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+
+    req.end();
+}
 app.listen(process.env.PORT || 3000)
