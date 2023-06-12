797c0944acca20b424fbcf3fd9c01a9ca9311d2a
fabiogunther <86040378+fabiogunther@users.noreply.github.com>
2023-06-12 21:38:46 +0000

    newnew

--- index.js
+++ index.js
@@ -10,23 +10,24 @@ app.get("/", (req, res) => {
   res.status(200).json({ error: 'FALSE', msg: "Bem vindo a API 2.0!" });
 });
 
-app.all('/getDouble/', (req, res) => {
-  let ret = buscarNextsLeap()
-  res.status(200).json({ error: 'FALSE', ret });
+app.all('/getDouble/', async (req, res) => {
+  let ret = await buscarNextsLeap(req, res)
+  //res.status(200).json({ error: 'FALSE', ret });
 })
 
-async function buscarNextsLeap() {
-  let urlPrincipal = 'https://lb.1x2networkhubmalta.com/f1x2games//virtualsport_fralis/getRacesDetailedJSON.jsp?sport_id=3&num_forward=20&install_id=1&siteID=4500'
-  await fetch(urlPrincipal)
-    .then((resposta) => resposta.json())
-    .then((dados) => {
-      //totalPaginas = dados.total_pages;
-      return dados;
-      //gravarRegistros(totalPaginas);
-    })
-    .catch((erro) => {
-      console.error("Erro ao obter dados", erro);
-    });
+async function buscarNextsLeap(req, res) {
+  const url =
+  'https://lb.1x2networkhubmalta.com/f1x2games//virtualsport_fralis/getRacesDetailedJSON.jsp?sport_id=3&num_forward=20&install_id=1&siteID=4500';
+	
+	
+	try {
+		let response = await fetch(url);
+		response = await response.json();
+		res.status(200).json(response);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({msg: `Internal Server Error.`});
+	}
 }
 
 app.listen(5000, () => {
