dcf6ea489b46a73a5547f272459acc0fd6a546b7
fabiogunther <86040378+fabiogunther@users.noreply.github.com>
2023-06-12 13:14:50 +0000

    getDouble

--- index.js
+++ index.js
@@ -71,6 +71,73 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
+
+app.all('/getDouble/', async (req, res) => {
+  await buscarPagina()
+  //res.send(result)
+})
+
+const urlPrincipal =
+"https://blaze.com/api/roulette_games/history?startDate=2023-05-09T00:00:00.000Z&endDate=2023-05-09T23:59:59.999Z&page=1";
+
+let objetoDados = [];
+let totalPaginas = 0;
+
+
+// await gravarRegistros(qtdPaginas);
+// await imprimir();
+
+// utilizado para buscar o total de páginas
+async function buscarPagina() {
+fetch(urlPrincipal)
+  .then((resposta) => resposta.json())
+  .then((dados) => {
+    totalPaginas = dados.total_pages;
+    console.log(totalPaginas);
+    gravarRegistros(totalPaginas);
+  })
+  .catch((erro) => {
+    console.error("Erro ao obter dados", erro);
+  });
+}
+
+async function gravarRegistros(paginas) {
+let url =
+  "https://blaze.com/api/roulette_games/history?startDate=2023-05-09T00:00:00.000Z&endDate=2023-05-09T23:59:59.999Z&page=";
+
+let teste = 0;
+for (let paginaAtual = 1; paginaAtual <= paginas; paginaAtual++) {
+  let urlCustomizada = url + paginaAtual;
+
+  fetch(urlCustomizada)
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+      //console.log(dados)
+      for (let i = 0; i < dados.records.length; i++) {
+        //console.log(dados.records[i])
+        objetoDados.push(dados.records[i]);
+      }
+      teste++;
+      imprimir(teste);
+    })
+    .catch((erro) => {
+      console.error("Erro ao obter dados", erro);
+    });
+}
+//return objetoDados;
+}
+
+async function imprimir(pag) {
+  onsole.log(pag);
+}
+
+
+
+
+
+
+
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
